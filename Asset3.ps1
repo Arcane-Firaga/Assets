53952adb4196f9e8d8b5927e42ce7f283e93cb99
Arcane-Firaga <61483363+Arcane-Firaga@users.noreply.github.com>
2020-02-25 20:33:01 -0500

    Add files via upload

---
+++ Asset3.ps1
@@ -0,0 +1,16 @@
+﻿Invoke-WebRequest -Uri "https://i.imgur.com/2hK7vSu.png" -OutFile "C:\photo.png"
+Function Set-WallPaper($Value)
+{
+Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "C:\photo.png";
+rundll32.exe user32.dll, UpdatePerUserSystemParameters .1 .True
+}
+Set-WallPaper -value "c:\\photo.png\"
+Set-WallPaper -value "c:\\photo.png\"
+Set-WallPaper -value "c:\\photo.png\"
+Set-WallPaper -value "c:\\photo.png\"
+Set-WallPaper -value "c:\\photo.png\"
+Set-WallPaper -value "c:\\photo.png\"
+Set-WallPaper -value "c:\\photo.png\"
+Set-WallPaper -value "c:\\photo.png\"
+del "C:\photo.png"
+exit
